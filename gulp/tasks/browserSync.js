c4d4dcd75ded1ad83fce5de9db62f63801468796
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2018-09-03 10:18:14 +0200

    Fix browserSync error

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -1,6 +1,6 @@
-const browserSync = require('browser-sync').create();
+const bs = require('browser-sync').create();
 const settings = require('../config/browserSync');
 
 module.exports = function browserSync() {
-    browserSync.init(settings.browserSync);
+    bs.init(settings.browserSync);
 };
