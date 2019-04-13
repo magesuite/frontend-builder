cc7013a8cf99c85c63ae9bb5f0f8255934715b8e
Mateusz Krzeszowiak <mateusz.krzeszowiak@creativestyle.pl>
2019-04-13 19:19:39 +0200

    Fix webpack cache patch

--- gulp/config/clean.js
+++ gulp/config/clean.js
@@ -9,5 +9,5 @@ module.exports = {
     /**
      * Paths that should be deleted.
      */
-    src: [path.join(paths.dist, '**/*'), path.join(paths.src, '.cache-loader')],
+    src: [path.join(paths.dist, '**/*'), path.resolve('.cache-loader')],
 };
