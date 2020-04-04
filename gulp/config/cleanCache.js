2034819fb0d9a8ceb881f94855bdbfa83350f775
Jacek Tomaszewski <jacek@jtom.me>
2020-04-04 14:52:31 +0200

    feat: In cleanCache task, allow to clean cache directly on a local machine as well (instead of through ssh connection)

--- gulp/config/cleanCache.js
+++ gulp/config/cleanCache.js
@@ -7,6 +7,29 @@ const paths = require('../paths');
  */
 module.exports = {
     cacheTypes: ['layout', 'block_html', 'full_page'],
+
+    /**
+     * magentoConnection: {
+     *   type: 'ssh',
+     *   host: string,
+     *   username: string,
+     *   privateKey: string,
+     *   path: string
+     * } | {
+     *   type: 'local',
+     *   path: string
+     * }
+     */
+    magentoConnection: {
+        type: 'ssh',
+        host: 'creativeshop.me',
+        username: 'magento2',
+        privateKey: path.resolve(
+            '../../../../../vagrant/ssh/creativeshop_vagrant.key'
+        ),
+        path: '/var/www/creativeshop/current',
+    },
+
     watch: [
         // Template files
         path.join(paths.dist, '**/*.{php,phtml,html,twig}'),
